285d82c4f11ec75b9f70504d225cad50783d560e
万冠呈 <41206703@qq.com>
2021-11-04 15:59:02 +0800

    test

---
+++ index1/js/main.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-04 08:18:30
+ * @LastEditTime: 2021-11-04 14:56:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Editfun
+ * @FilePath: \Zero-Cup-2021\index1\js\main.js
+ */
+// let contents = document.querySelectorAll('.content');
+// for (item of contents) {
+//     item.onmouseover = function() {
+//         this.style.opacity = '1';
+//         this.style.visibility = "visible";
+//         console.log("1")
+//     }
+// }
+// console.log("hi");
+
+// $('.timecard').hover(function(){
+//     $('i')
+// }
+// )
+
+$(".timecard").hover(function (e) {
+    console.log(e);
+    console.log(e.currentTarget.classList[1]);
+    $(".content." + e.currentTarget.classList[1]).addClass("hover");
+    console.log(e.currentTarget.classList);
+}, function (e) {
+    $(".content."+ e.currentTarget.classList[1]).removeClass("hover");
+});
