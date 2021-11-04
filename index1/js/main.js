ad4e86a052403e4adb451e63f5270c97c3fb6b53
万冠呈 <41206703@qq.com>
2021-11-04 21:07:26 +0800

    indexed

--- index1/js/main.js
+++ index1/js/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-04 08:18:30
- * @LastEditTime: 2021-11-04 19:08:07
+ * @LastEditTime: 2021-11-04 20:54:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Editfun
  * @FilePath: \Zero-Cup-2021\index1\js\main.js
@@ -32,3 +32,25 @@ $(".timecard").hover(function (e) {
 
 
 $(".timecard:odd").css("right","-140vh");
+
+
+$('.timecard:eq(0)').click(function (){
+    window.location.href="http://zhidao.baidu.com/question/141095092468292125.html"
+})
+
+$('.timecard:eq(1)').click(function (){
+    window.location.href="https://www.zhihu.com/question/319535726/answer/647773410"
+})
+
+$('.timecard:eq(2)').click(function (){
+    window.location.href="https://zhuanlan.zhihu.com/p/23527927"
+})
+$('.timecard:eq(3)').click(function (){
+    window.location.href="https://zhuanlan.zhihu.com/p/72003260"
+})
+$('.timecard:eq(4)').click(function (){
+    window.location.href="https://www.zhihu.com/question/55608987/answer/151380948"
+})
+$('.timecard:eq(5)').click(function (){
+    window.location.href="https://www.zhihu.com/question/36979454/answer/1090443746"
+})
