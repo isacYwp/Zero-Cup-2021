322a99652bd95d3198b12782cfbebc9d9bf329ce
万冠呈 <41206703@qq.com>
2021-11-05 17:03:05 +0800

    final

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-02 20:17:12
- * @LastEditTime: 2021-11-05 14:23:19
+ * @LastEditTime: 2021-11-05 16:55:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Zero-Cup-2021\js\index.js
@@ -36,6 +36,19 @@ document.addEventListener("scroll", () => {
   } else {
     background.style.backgroundPosition = "";
   }
+  if (scroll <= 1548) {
+   
+    $('.letter1').css('opacity', '0');
+    $('.letter2').css('opacity', '0');
+    $('.letter3').css('opacity', '0');
+  } else {
+   console.log('yoooooooooooooooooooooooooo!')
+   $('.letter1').css('opacity', '1');
+   $('.letter2').css('opacity', '1');
+   $('.letter3').css('opacity', '1');
+  }
 });
+
+
 
 }
